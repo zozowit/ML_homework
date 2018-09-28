fcf9169c7a0954624765f498ed9c379a9d6a9bc2
zozowit <zozowit@126.com>
2018-09-28 10:57:00 +0800

    1

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -35,12 +35,15 @@ load ('ex5data1.mat');
 m = size(X, 1);
 
 % Plot training data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% Useful properties to modify are "linestyle",
+%     "linewidth", "color", "marker", "markersize", "markeredgecolor",
+%     "markerfacecolor".
+plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 15);
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+% pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 %  You should now implement the cost function for regularized linear 
@@ -54,7 +57,7 @@ fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+% pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
 %  You should now implement the gradient for regularized linear 
@@ -69,7 +72,7 @@ fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          grad(1), grad(2));
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+% pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
@@ -90,11 +93,12 @@ plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 hold on;
+% plot the H(x)
 plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
 hold off;
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+% pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
@@ -109,7 +113,9 @@ lambda = 0;
     learningCurve([ones(m, 1) X], y, ...
                   [ones(size(Xval, 1), 1) Xval], yval, ...
                   lambda);
-
+fprintf("m is %d\n", m)
+% plot(1:m, error_train, 1:m, error_val);
+% plot(x, y1, x, y2)
 plot(1:m, error_train, 1:m, error_val);
 title('Learning curve for linear regression')
 legend('Train', 'Cross Validation')
